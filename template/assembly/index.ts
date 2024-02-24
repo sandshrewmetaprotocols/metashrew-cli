f57e16cb6e9ffd4489627eefeee484f3a9eed4b6
sandshrewmetadev <inquiries@sandshrew.io>
2024-02-24 20:00:21 +0000

    update

--- template/assembly/index.ts
+++ template/assembly/index.ts
@@ -3,9 +3,12 @@ import { input } from "metashrew-as/assembly/indexer/index";
 import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
 import { Block } from "metashrew-as/assembly/blockdata/block";
 
+import { console } from "metashrew-as/assembly/utils/logging";
+
 export function _start(): void {
   const data = input();
   const box = Box.from(data);
   const height = parsePrimitive<u32>(box);
   const block = new Block(box);
+  console.log("got block " + height.toString(10));
 }
