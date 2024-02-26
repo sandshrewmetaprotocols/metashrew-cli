ff0e2bc5e1751ea97aa751600a2a4748e2307bdb
Ubuntu <ubuntu@ip-172-31-40-126.us-east-2.compute.internal>
2024-02-26 14:07:34 +0000

    update index

--- template/assembly/index.ts
+++ template/assembly/index.ts
@@ -1,5 +1,5 @@
 import { Box } from "metashrew-as/assembly/utils/box"
-import { input, get, set, Index } from "metashrew-as/assembly/indexer/index";
+import { _flush, input, get, set, Index } from "metashrew-as/assembly/indexer/index";
 import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
 import { Block } from "metashrew-as/assembly/blockdata/block";
 import { Transaction, Input, Output } from "metashrew-as/assembly/blockdata/transaction";
@@ -13,4 +13,5 @@ export function _start(): void {
   const height = parsePrimitive<u32>(box);
   const block = new Block(box);
   console.log("got block " + height.toString(10));
+  _flush();
 }
